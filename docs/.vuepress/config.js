fc6c4529951e48e8a49e3f7f52f25cd1390e730b
james9527 <625601613@qq.com>
2021-03-11 23:33:48 +0800

    feat: 修改入口文件路径

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,8 +22,14 @@ module.exports = {
     navBar: true,
     // 显示所有页面的标题链接，默认情况下，侧边栏只会显示由当前活动页面的标题链接
     displayAllHeaders: true,
+    // 启用页面滚动效果
+    smoothScroll: true,
+    // 启用上一篇链接
+    prevLinks: true,
+    // 启用下一篇链接
+    nextLinks: true,
     nav: [{
-        text: '随手记系列',
+        text: '前端基础',
         items: [{
             text: '前端基础JS系列',
             link: '/blog/share/javascript-basic'
@@ -59,6 +65,43 @@ module.exports = {
         ]
       },
       {
+        text: '前端进阶',
+        items: [
+          {
+            text: 'eslint规则',
+            link: '/blog/share/eslint'
+          },
+          {
+            text: '性能优化',
+            link: '/blog/share/opt'
+          },
+          {
+            text: '深入webpack',
+            link: '/blog/share/webpack'
+          },
+          {
+            text: 'webpack随手记',
+            link: '/blog/share/webpack-note'
+          },
+          {
+            text: '公众号开发总结',
+            link: '/blog/share/wechat'
+          },
+          {
+            text: 'markdown语法',
+            link: '/blog/share/md'
+          },
+        ]
+      },
+      {
+        text: '前端工程化',
+        items: [{
+            text: 'webpack 英文',
+            link: '/blog/en/webpack'
+          },
+        ]
+      },
+      {
         text: '工作相关',
         items: [{
             text: '深入skulpt',
@@ -71,42 +114,15 @@ module.exports = {
         ]
       },
       {
-        text: '计算机英语',
-        items: [{
-            text: 'webpack 英文',
-            link: '/blog/en/webpack'
-          },
-          {
-            text: 'npm 英文',
-            link: '/blog/en/npm'
-          },
-          {
-            text: 'skulpt 英文',
-            link: '/blog/en/skulpt'
-          },
-          {
-            text: 'scratch 英文',
-            link: '/blog/en/scratch'
-          },
-          {
-            text: 'ecma 英文',
-            link: '/blog/en/ecma'
-          },
-        ]
-      },
-      {
         text: '常用网址',
-        link: '/blog/url/fe'
+        link: '/blog/bookmark/home'
       },
       {
         text: 'github',
         link: 'https://github.com/james9527/blog'
       },
     ],
-    sidebar: {
-      '/skulpt/': genSidebarConfig('skulpt', ['links', '1']),
-      '/scratch/': genSidebarConfig('深入scratch', ['links', '1', 'sb3']),
-    },
+    sidebar: false,
     lastUpdated: "更新时间",
     docsDir: "docs",
     editLinks: true,
